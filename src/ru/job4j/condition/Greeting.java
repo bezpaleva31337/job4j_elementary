d093698aa909da278cf553dbec0276d56116ca16
bezpaleva31337 <leadsystem@inbox.ru>
2021-02-09 01:43:54 +0300

    9.1. String [#461602]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        String id = idea + "But I am a newbie.";
+        System.out.println(id);
+        int year = 2021;
+        String ideaPlusYear = idea + year;
+        System.out.println(ideaPlusYear);
+
+    }
+}
